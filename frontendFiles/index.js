1ba1ce17074be204788887eac362a3ec75f0aef0
ChristopherE7 <kikoskaf@gmail.com>
2023-07-16 15:57:51 -0400

    changed js to simpler code to pick which button

---
+++ frontendFiles/index.js
@@ -0,0 +1,27 @@
+// const loginBtn = document.querySelector("#loginBtn");
+// const registerBtn = document.querySelector("#registerBtn");
+//
+// if (registerBtn) {
+// }
+
+const buttons = document.querySelectorAll(".custom-button");
+
+// Add event listener to all buttons
+buttons.forEach((button) => {
+  button.addEventListener("click", handleButtonClick);
+});
+
+// Event listener function for button clicks
+function handleButtonClick(event) {
+  const button = event.target;
+  const buttonType = button.getAttribute("data-button");
+
+  // Perform actions based on button type
+  if (buttonType === "loginBtn") {
+    console.log("Login");
+    // Perform actions for Button 1
+  } else if (buttonType === "registerBtn") {
+    console.log("Register");
+    // Perform actions for Button 2
+  }
+}
